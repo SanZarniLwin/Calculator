80a9cd3604fb496ef1f399fb3700880ff64a88ad
SanZarniLwin <sangyi02.sg@gmail.com>
2025-08-11 16:44:44 +0700

    Fixing depend on comments

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,17 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  String displayText = '';
+  String displayText = '0';
   double firstOperand = 0;
   String selectedOperator = '';
 
   void onNumberPressed(String number) {
     setState(() {
-      displayText += number;
+      if (displayText == '0') {
+        displayText = number;
+      } else {
+        displayText += number;
+      }
     });
   }
 
@@ -26,7 +30,7 @@ class _CalculatorState extends State<Calculator> {
     setState(() {
       firstOperand = double.tryParse(displayText) ?? 0;
       selectedOperator = operator;
-      displayText = '';
+      displayText = '0';
     });
   }
 
@@ -63,7 +67,7 @@ class _CalculatorState extends State<Calculator> {
 
   void clearAll() {
     setState(() {
-      displayText = '';
+      displayText = '0';
       firstOperand = 0;
       selectedOperator = '';
     });
@@ -75,8 +79,8 @@ class _CalculatorState extends State<Calculator> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.yellowAccent,
-          brightness: Brightness.light,
+          seedColor: Colors.yellow,
+          brightness: Brightness.dark,
         ),
       ),
       home: Scaffold(
@@ -88,73 +92,193 @@ class _CalculatorState extends State<Calculator> {
           children: [
             Expanded(
               child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                  color: Colors.yellow[100],
+                ),
                 alignment: Alignment.bottomRight,
                 padding: const EdgeInsets.all(30),
                 child: Text(
                   displayText,
-                  style: const TextStyle(fontSize: 32),
+                  style: const TextStyle(fontSize: 52,
+                  color: Colors.black),
                 ),
               ),
             ),
             Row(
+              
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('1'), child: Text('1')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('2'), child: Text('2')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('3'), child: Text('3')),
-                ElevatedButton(
-                    onPressed: () => onOperatorPressed('+'), child: Text('+')),
+                Expanded(
+                    child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                            onPressed: () => onNumberPressed('1'), child: Text('1', style: TextStyle(fontSize: 30))),
+                    ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('2'), child: Text('2', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('3'), child: Text('3', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onOperatorPressed('+'), child: Text('+', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
               ],
             ),
-            SizedBox(
-              height: 20,
+            Container(
+              color: Colors.yellow[100],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('4'), child: Text('4')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('5'), child: Text('5')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('6'), child: Text('6')),
-                ElevatedButton(
-                    onPressed: () => onOperatorPressed('-'), child: Text('-')),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('4'), child: Text('4', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('5'), child: Text('5', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('6'), child: Text('6', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onOperatorPressed('-'), child: Text('-', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
               ],
             ),
-            SizedBox(
-              height: 20,
+            Container(
+              color: Colors.yellow[100],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('7'), child: Text('7')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('8'), child: Text('8')),
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('9'), child: Text('9')),
-                ElevatedButton(
-                    onPressed: () => onOperatorPressed('x'), child: Text('x')),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('7'), child: Text('7', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('8'), child: Text('8', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onNumberPressed('9'), child: Text('9', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                    child: ElevatedButton(
+                        onPressed: () => onOperatorPressed('x'), child: Text('x', style: TextStyle(fontSize: 30))),
+                  ),
+                ),
               ],
             ),
-            SizedBox(
-              height: 20,
+            Container(
+              color: Colors.yellow[100],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                    onPressed: () => onNumberPressed('0'), child: Text('0')),
-                ElevatedButton(
-                    onPressed: () => onOperatorPressed('/'), child: Text('/')),
-                ElevatedButton(onPressed: clearAll, child: const Text('C')),
-                ElevatedButton(
-                    onPressed: calculateResult, child: const Text('=')),
+                Expanded(
+                    child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                          onPressed: () => onNumberPressed('0'), child: Text('0', style: TextStyle(fontSize: 30))),
+                    ),
+                ),
+                Expanded(
+                    child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                          onPressed: () => onOperatorPressed('/'), child: Text('/', style: TextStyle(fontSize: 30))),
+                    ),
+                ),
+                Expanded(
+                  child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),child: ElevatedButton(onPressed: clearAll, child: const Text('C', style: TextStyle(fontSize: 30)))),
+                ),
+                Expanded(
+                    child: Container(
+                      height: 70,
+                      color: Colors.yellow[100],
+                      padding: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                          onPressed: calculateResult, child: const Text('=', style: TextStyle(fontSize: 30))),
+                    ),
+                ),
               ],
+            ),
+            Container(
+              color: Colors.yellow[100],
+              padding: EdgeInsets.all(20),
             ),
           ],
         ),
